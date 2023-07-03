5f7f656ecb247237a453be69594fe13f5bc66497
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-03 21:15:24 +0200

    move contact page from "/contacts" to "/" path

--- src/Controller/ContactsController.php
+++ src/Controller/ContactsController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacts;
+use App\Form\ContactsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactsController extends AbstractController
 {
-    #[Route('/contacts', name: 'app_contacts')]
+    #[Route('/', name: 'app_contacts')]
     public function index(): Response
     {
+
+        $contacts = new Contacts();
+
+        $form = $this->createForm(ContactsType::class, $contacts);
+
         return $this->render('contacts/index.html.twig', [
-            'controller_name' => 'ContactsController',
+            'contactForm' => $form->createView(),
         ]);
     }
 }
