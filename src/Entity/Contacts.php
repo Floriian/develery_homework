23367d35df2f31ed54938959518e6246c9af0e51
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-05 17:01:00 +0200

    add createdAt field to Contacts.

--- src/Entity/Contacts.php
+++ src/Entity/Contacts.php
@@ -30,6 +30,9 @@ class Contacts
     #[Assert\Length(max: 255)]
     private ?string $message = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -67,6 +70,18 @@ class Contacts
     public function setMessage(string $message): static
     {
         $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
